2479ebf88c2b3b7ff3854cc5d21c070294bf9e4c
achorn <joshuaachorn@gmail.com>
2023-06-08 15:37:19 -0600

    added page for entry and query with document id

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Entries from "./pages/MyEntries";
+import Entry from "./pages/Entry";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
@@ -16,6 +17,7 @@ function App() {
             <Route exact path={"/signup"} element={<SignUp />} />
             <Route exact path={"/signin"} element={<SignIn />} />
             <Route exact path={"/entries"} element={<Entries />} />
+            <Route path={"entry/:id"} element={<Entry />} />
           </Routes>
         </Router>
       </AuthProvider>
---
+++ src/pages/Entry.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import { doc, query, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase/config";
+
+const Entry = () => {
+  const [post, setPost] = useState({});
+  const { id } = useParams();
+  const [loadingPost, setIsLoadingPost] = useState(false);
+
+  useEffect(() => {
+    let unsubscribe;
+    setIsLoadingPost(true);
+    try {
+      const q = query(doc(db, "entries", id));
+      unsubscribe = onSnapshot(q, (doc) => {
+        let newData = {};
+        newData = {
+          ...doc.data(),
+          // key: doc.id,
+        };
+        setPost(newData);
+        setIsLoadingPost(false);
+      });
+    } catch (error) {
+      console.log(error);
+      setIsLoadingPost(false);
+    }
+    return () => unsubscribe();
+  }, [id]);
+
+  if (loadingPost) {
+    return <div>Loading...</div>;
+  } else if (JSON.stringify(post) === "{}") {
+    return <div>no post here...</div>;
+  } else {
+    return (
+      <div>
+        <h1>{"Entry"}</h1>
+        <h2>{post.title}</h2>
+        <h3>{post.content}</h3>
+      </div>
+    );
+  }
+};
+
+export default Entry;
